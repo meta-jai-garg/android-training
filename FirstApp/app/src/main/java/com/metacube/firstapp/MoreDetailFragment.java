7741de4a010c4639caaa36f07c668bb73b38514c
dev-jk <jai.garg@metacube.com>
2019-01-17 22:37:21 +0530

    Grid View with Footer

--- FirstApp/app/src/main/java/com/metacube/firstapp/MoreDetailFragment.java
+++ FirstApp/app/src/main/java/com/metacube/firstapp/MoreDetailFragment.java
@@ -12,7 +12,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MoreDetailFragment extends Fragment implements View.OnClickListener, OnScroll {
+public class MoreDetailFragment extends Fragment implements View.OnClickListener {
 
     private GridView gridView;
     private ImageView facebookLinkImg, instagramLinkImg;
@@ -21,12 +21,12 @@ public class MoreDetailFragment extends Fragment implements View.OnClickListener
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.frag_more, container, false);
+        View view = inflater.inflate(R.layout.fragment_more_detail, container, false);
         gridView = view.findViewById(R.id.moreDetailsGridView);
-//        facebookLinkImg = view.findViewById(R.id.facebookLinkImg);
-//        instagramLinkImg = view.findViewById(R.id.instagramLinkImg);
-//        facebookLinkImg.setOnClickListener(this);
-//        instagramLinkImg.setOnClickListener(this);
+        facebookLinkImg = view.findViewById(R.id.facebookLinkImg);
+        instagramLinkImg = view.findViewById(R.id.instagramLinkImg);
+        facebookLinkImg.setOnClickListener(this);
+        instagramLinkImg.setOnClickListener(this);
         gridView.setAdapter(new ImageAdapter(getContext()));
         gridView.setNumColumns(2);
         gridView.setOnScrollListener(new AbsListView.OnScrollListener() {
